803d749b1ece04fae572e242d9d2cce4a781dfd2
Hugo <hugo@42devs.cl>
2021-09-30 01:23:17 -0300

    filerouter: added base and fallback route

---
+++ src/pages/_.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+router-link(to="/")
+  p 404!
+</template>
+
+<script>
+export default {
+  name: '404',
+};
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+p Hi i am the base route!
+</template>
